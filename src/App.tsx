d6516f47c4e040bade7c8a0fece7044409610190
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-09-06 15:23:09 +0300

    PizzaBlock suport useState . Fetch connect server.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/header/Header';
 import Content from './components/content/Content';
 import { Route } from 'react-router-dom';
@@ -6,11 +6,21 @@ import Carts from './components/carts/Carts';
 
 
 let App = () => {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((json) => { setPizzas(json.pizzas) })
+  }, [])
+
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path='/' component={Content} />
+        <Route exact path='/' render={() => <Content items={pizzas} />} />
         <Route exact path='/carts' component={Carts} />
       </div>
     </div>
